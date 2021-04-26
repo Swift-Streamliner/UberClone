f1e7259bb8ac7f5fef8bdf68f7ba0cd5159517da
thisisstreamliner <thisisstreamliner@gmail.com>
2021-04-26 10:32:21 +0300

    Setup Project

---
+++ UberClone/Controller/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  UberClone
+//
+//  Created by Tiger Mei on 26.04.2021.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
